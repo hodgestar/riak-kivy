c47b5f1a3f0d25aac6529f24ad99a967faa0d08c
Simon Cross <hodgestar@gmail.com>
2012-04-23 19:14:52 +0200

    Factor out common code and clean-up stuff.

--- briak/main.py
+++ briak/main.py
@@ -7,70 +7,83 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.scrollview import ScrollView
-from kivy.logger import Logger
 
 
-def create_test_data(client):
-    r = RiakObject(client, client.bucket("test.bucket.1"))
-    r.set_data({"foo": 1, "bar": "baz"})
-    r.store()
-
-    r = RiakObject(client, client.bucket("test.bucket.2"))
-    r.set_data({"foo": 2, "bar": "baz"})
-    r.store()
+def create_test_data(client, n):
+    for i in range(n):
+        r = RiakObject(client, client.bucket("test.bucket.%d" % i))
+        r.set_data({"foo": i, "bar": "baz"})
+        r.store()
 
 
 class AppWidget(Widget):
     def __init__(self, client):
         super(AppWidget, self).__init__()
         self.client = client
+        create_test_data(client, 6)
         self.bucket_list = BucketList(self)
         self.key_list = KeyList(self)
         self.key_data = KeyData(self)
-        self.switch_to_bucket_list()
+
+    def set_widget(self, widget):
+        self.clear_widgets()
+        self.add_widget(widget)
+        window = self.get_parent_window()
+        widget.size = window.size
 
     def switch_to_bucket_list(self):
-        self.clear_widgets()
-        self.add_widget(self.bucket_list)
+        self.bucket_list.refresh()
+        self.set_widget(self.bucket_list)
 
     def switch_to_bucket(self, bucket):
-        self.clear_widgets()
         self.key_list.set_bucket(bucket)
-        self.add_widget(self.key_list)
+        self.set_widget(self.key_list)
 
     def switch_to_key(self, bucket, key):
-        self.clear_widgets()
         self.key_data.set_key(bucket, key)
-        self.add_widget(self.key_data)
+        self.set_widget(self.key_data)
 
 
-class BucketList(StackLayout):
+class AppList(StackLayout):
+
     def __init__(self, app):
-        super(BucketList, self).__init__()
+        super(AppList, self).__init__()
         self.bind(minimum_height=self.setter('height'))
         self.app = app
 
-        title = Label(text="Riak Buckets")
-        self.add_widget(title)
+        self.title = Label(text="No title yet", height=30)
+        self.add_widget(self.title)
 
-        refresh = Button(text="Refresh")
-        refresh.bind(on_press=self.refresh)
-        self.add_widget(refresh)
+        self.add_buttons()
 
         scroll = ScrollView()
         self.add_widget(scroll)
 
-        self.layout = StackLayout(size_hint_y=None)
+        self.layout = GridLayout(cols=1, size_hint_y=None)
         self.layout.bind(minimum_height=self.layout.setter('height'))
         scroll.add_widget(self.layout)
 
-        self.refresh(None)
+    def add_buttons(self):
+        pass
 
-    def refresh(self, _refresh_btn):
+
+class BucketList(AppList):
+    def __init__(self, app):
+        super(BucketList, self).__init__(app)
+        self.title.text = "Riak Buckets"
+
+    def add_buttons(self):
+        refresh = Button(text="Refresh", height=30)
+        refresh.bind(on_press=self.refresh)
+        self.add_widget(refresh)
+
+    def refresh(self, _refresh_btn=None):
         self.layout.clear_widgets()
         buckets = self.app.client.get_buckets()
+        buckets.sort()
         if not buckets:
             self.layout.add_widget(Label(text="Can has bucket?"))
         for bucket in buckets:
@@ -84,30 +97,20 @@ class BucketList(StackLayout):
         self.app.switch_to_bucket(bucket)
 
 
-class KeyList(StackLayout):
+class KeyList(AppList):
     def __init__(self, app):
-        super(KeyList, self).__init__()
-        self.bind(minimum_height=self.setter('height'))
-        self.app = app
+        super(KeyList, self).__init__(app)
+
+    def add_buttons(self):
         self.bucket = None
 
-        self.title = Label(text="No bucket yet")
-        self.add_widget(self.title)
-
-        back_button = Button(text="Back ...")
+        back_button = Button(text="Back ...", height=30)
         back_button.bind(on_press=self.back)
         self.add_widget(back_button)
 
-        refresh = Button(text="Refresh")
+        refresh = Button(text="Refresh", height=30)
         refresh.bind(on_press=self.refresh)
         self.add_widget(refresh)
-
-        scroll = ScrollView()
-        self.add_widget(scroll)
-
-        self.layout = StackLayout(size_hint_y=None)
-        self.layout.bind(minimum_height=self.layout.setter('height'))
-        scroll.add_widget(self.layout)
 
     def back(self, _back_btn):
         self.app.switch_to_bucket_list()
@@ -125,6 +128,7 @@ class KeyList(StackLayout):
         self.layout.clear_widgets()
 
         keys = bucket.get_keys()
+        keys.sort()
         if not keys:
             self.layout.add_widget(Label(text="Can has key?"))
         for key in keys:
@@ -138,30 +142,20 @@ class KeyList(StackLayout):
         self.app.switch_to_key(button.bucket, button.key)
 
 
-class KeyData(StackLayout):
+class KeyData(AppList):
     def __init__(self, app):
-        super(KeyData, self).__init__()
-        self.bind(minimum_height=self.setter('height'))
-        self.app = app
+        super(KeyData, self).__init__(app)
+
+    def add_buttons(self):
         self.bucket, self.key = None, None
 
-        self.title = Label(text="No key yet")
-        self.add_widget(self.title)
-
-        back_button = Button(text="Back ...")
+        back_button = Button(text="Back ...", height=30)
         back_button.bind(on_press=self.back)
         self.add_widget(back_button)
 
-        refresh = Button(text="Refresh")
+        refresh = Button(text="Refresh", height=30)
         refresh.bind(on_press=self.refresh)
         self.add_widget(refresh)
-
-        scroll = ScrollView()
-        self.add_widget(scroll)
-
-        self.layout = StackLayout(size_hint_y=None)
-        self.layout.bind(minimum_height=self.layout.setter('height'))
-        scroll.add_widget(self.layout)
 
     def back(self, _back_btn):
         self.app.switch_to_bucket(self.bucket)
@@ -184,7 +178,7 @@ class KeyData(StackLayout):
 
         if not data:
             self.layout.add_widget(Label(text="Can has data?"))
-        for key, value in data.items():
+        for key, value in sorted(data.items(), key=lambda t: t[0]):
             label = Label(text="%s: %s" % (key, value))
             self.layout.add_widget(label)
 
@@ -195,7 +189,12 @@ class MyApp(App):
 
     def build(self):
         client = RiakClient()
-        return AppWidget(client)
+        self.app = AppWidget(client)
+        return self.app
+
+    def on_start(self):
+        super(MyApp, self).on_start()
+        self.app.switch_to_bucket_list()
 
 
 if __name__ == '__main__':
